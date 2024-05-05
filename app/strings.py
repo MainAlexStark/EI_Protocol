996860ee6896b50b72c2a4e1c8beb5c78a51c498
MainAlexStark <143014347+MainAlexStark@users.noreply.github.com>
2024-05-05 04:31:06 +0300

    Delete app directory

--- app/strings.py
+++
@@ -1,3 +0,0 @@
-start_text = '\nProgram: EI_Protocol\nVersion: alpha 2.1 \
-                     \nThis program is created for commercial use\
-                     \nThe rights to the program belong to the company: ООО "Единица Измерения"\n'
--- app/tools/parse/__init__.py
+++
@@ -1,2 +0,0 @@
-from .FNS_RUS import FNS_API
-from .LIST_ORG import List_org
